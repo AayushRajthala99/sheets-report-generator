b2261007657285b1d7bdc0c46cfd585237dfbbdc
Aayush Rajthala <Aayushrajthala@gmail.com>
2023-01-16 12:22:21 +0545

    Initialized Project Code

---
+++ report.py
@@ -0,0 +1,263 @@
+import csv
+import json
+import time
+import numpy as np
+from os import path
+import pandas as pd
+from sheet_writer import sharer, writer
+from service_creator import Create_Service
+from service_creator import Create_Service2
+
+# --------------------------------------------------------------------------------------------
+
+CLIENT_SECRET_FILE = '.\credentials\client-secret.json'
+SERVICE_ACCOUNT_FILE = '.\credentials\service-account.json'
+API_NAME = 'drive'
+API_VERSION = 'v3'
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+# subDirectories = ['Enter the directories to be listed in the report']
+
+# Example of subDirectories...
+subDirectories = ['recordings', 'responses',
+                  'screenshots', 'packetcaptures', 'urlInfo.csv']
+
+# Drive ID of the Shared Drive... Change this to choose the Directory to upload the files to...
+driveID = '--Enter The Drive ID Here--'
+
+# Parent Directory ID of the shared drive....
+parent_directory_id = '--Enter The Parent Directory ID Here--'
+
+# Spreadsheet ID to populate the results...
+spreadsheet_id = '--Enter The Spreadsheet ID Here--'
+
+# ----------------------------------------------------------------------------------------------------------------------------------
+
+# service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+service = Create_Service2(SERVICE_ACCOUNT_FILE, API_NAME, API_VERSION, SCOPES)
+
+
+def idreturner(id, array):
+    try:
+        for item in array:
+            if f"{id}" in item['name']:
+                return item['id']
+    except Exception as error:
+        print("Error in idreturner Function::: ", error)
+        pass
+
+
+def lister(service, driveId, parentId, sleeptime=None):
+
+    if (sleeptime):
+        time.sleep(int(sleeptime))
+
+    try:
+        query = (f"parents ='{parentId}' and trashed=False")
+
+        response = service.files().list(q=query, fields="files(id, name, mimeType)", orderBy='name', includeItemsFromAllDrives=True,
+                                        corpora='drive', driveId=driveId, supportsAllDrives=True).execute()
+
+        files = response.get('files')
+        df = pd.DataFrame(files)
+
+        # Replacing all NaN values with 'N/A'...
+        df = df.replace(np.nan, 'N/A')
+
+        df = df.to_dict('records')
+        return (df)
+
+    except Exception as error:
+        print("Error in lister Function::: ", error)
+        pass
+
+
+def directoryInfo(directoryObject, directoryName):
+    try:
+        value = [obj for obj in directoryObject if directoryName in obj['name']]
+
+        if (len(value) > 0):
+            return value[0]
+        else:
+            return None
+
+    except Exception as error:
+        print("Error in directoryInfo Function::: ", error)
+        pass
+
+
+def subDirectoryInfo(parentDirectory):
+    try:
+        test_name = parentDirectory['name']
+        test_folder_id = parentDirectory['id']
+        # test_name = directory['name']
+        # print("\nTest Name:::", test_name)
+
+        # Getting Sub-Directories & Files within Test Results [recordings,responses,screenshots,packetcaptures,urlInfo.csv]
+        test_subDirectories = lister(service, driveID, test_folder_id, 1)
+
+        recordingInfo = directoryInfo(test_subDirectories, subDirectories[0])
+        if (recordingInfo):
+            recordingInfo = recordingInfo['id']
+        # print("Recording Directory ID:::", recordingInfo)
+
+        responseInfo = directoryInfo(test_subDirectories, subDirectories[1])
+        if (responseInfo):
+            responseInfo = responseInfo['id']
+        # print("Response Directory ID:::", responseInfo)
+
+        screenshotInfo = directoryInfo(test_subDirectories, subDirectories[2])
+        if (screenshotInfo):
+            screenshotInfo = screenshotInfo['id']
+        # print("Screenshot Directory ID:::", screenshotInfo)
+
+        pcapInfo = directoryInfo(test_subDirectories, subDirectories[3])
+        if (pcapInfo):
+            pcapInfo = pcapInfo['id']
+        # print("Packet Capture Directory ID:::", pcapInfo)
+
+        urlInfo = directoryInfo(test_subDirectories, subDirectories[4])
+        if (urlInfo):
+            urlInfo = urlInfo['id']
+        # print("Packet Capture Directory ID:::", urlInfo)
+
+        resultObject = {
+            "test_name": test_name,
+            "test_folder_id": test_folder_id,
+            "recordingFolderID": recordingInfo,
+            "responseFolderID": responseInfo,
+            "screenshotFolderID": screenshotInfo,
+            "pcapFolderID": pcapInfo,
+            "urlInfoID": urlInfo
+        }
+
+        return resultObject
+
+    except Exception as error:
+        print("Error in subDirectoryInfo Function::: ", error)
+        pass
+
+
+resultDirectories = lister(service, driveID, parent_directory_id)
+# print(resultDirectories)
+
+if path.isfile('.\credentials\generatedReports.txt') is False:
+    print("--generatedReports.txt File Created!--")
+    File = open('.\credentials\generatedReports.txt', "x")
+    File.close()
+
+reportFile = open('.\credentials\generatedReports.txt', 'rb')
+alreadyGeneratedReportList = reportFile.read().splitlines()
+reportFile.close()
+
+for i, value in enumerate(alreadyGeneratedReportList):
+    if (len(value) > 0):
+        alreadyGeneratedReportList[i] = value.decode('ASCII')
+# print(alreadyGeneratedReportList)
+
+reportGenerationCount = 0
+generatedReports = []
+
+for directory in resultDirectories:
+    try:
+        directoryID = str(directory["id"])
+        if (not (directoryID in alreadyGeneratedReportList) and (directory["mimeType"] == 'application/vnd.google-apps.folder')):
+            reportGenerationCount += 1
+            testDirectory = subDirectoryInfo(directory)
+
+            # Gathering IDs for further Operation...
+            testDirectoryID = testDirectory['test_folder_id']
+            test_name = testDirectory['test_name']
+            recordingFolderID = testDirectory['recordingFolderID']
+            responseFolderID = testDirectory['responseFolderID']
+            screenshotFolderID = testDirectory['screenshotFolderID']
+            pcapFolderID = testDirectory['pcapFolderID']
+            urlInfoID = testDirectory['urlInfoID']
+
+            # Retrieving List of Files in Respective Directories...
+            recordingList = lister(service, driveID, recordingFolderID)
+            responseList = lister(service, driveID, responseFolderID)
+            screenshotList = lister(service, driveID, screenshotFolderID)
+            pcapList = lister(service, driveID, pcapFolderID)
+
+            print("\nTest Name::: [ {0} ]".format(test_name))
+            # print("\nRecording List:::", recordingList)
+            # print("\nResponse List:::", responseList)
+            # print("\nScreenshot List:::", screenshotList)
+            # print("\nPCAP List:::", pcapList)
+
+            # urlInfoCSVLink = sharer(urlInfoID)
+            # print("urlInfo.csv LINK:::", urlInfoCSVLink)
+
+            csvReadURL = f"https://drive.google.com/uc?id={urlInfoID}"
+            resultDataframe = pd.read_csv(csvReadURL, encoding='utf8')
+            resultDataframe = resultDataframe.replace(np.nan, '')
+            resultDataframe = resultDataframe.to_dict('records')
+
+            print(
+                "\n--Started Link Generation for Recording, Response, Screenshot & Packet Capture Files--")
+
+            for result in resultDataframe:
+                try:
+                    print(
+                        "\n--Generating Links for [ {0} ]--".format(result['id']), end='')
+
+                    # Gathering File IDs...
+                    recordingID = idreturner(result['id'], recordingList)
+                    responseID = idreturner(result['id'], responseList)
+                    screenshotID = idreturner(result['id'], screenshotList)
+                    pcapID = idreturner(result['id'], pcapList)
+
+                    # Generating shareable links for above File IDs...
+                    if (recordingID):
+                        result['Recording Link'] = sharer(recordingID)
+                    else:
+                        result['Recording Link'] = 'N/A'
+
+                    if (responseID):
+                        result['Response Link'] = sharer(responseID)
+                    else:
+                        result['Response Link'] = 'N/A'
+
+                    if (screenshotID):
+                        result['Screenshot Link'] = sharer(screenshotID)
+                    else:
+                        result['Screenshot Link'] = 'N/A'
+
+                    if (pcapID):
+                        result['PCAP Link'] = sharer(pcapID)
+                    else:
+                        result['PCAP Link'] = 'N/A'
+
+                    print(">> DONE")
+
+                except Exception as error:
+                    print("Error in Generating Link For [ {0} ]::: {1}".format(
+                        result['id'], error))
+                    pass
+
+            print("\n--Link Generation Operation Ended--")
+            # print(resultDataframe)
+
+            # Extracted Headers for Spreadsheet...
+            headers = list(resultDataframe[0].keys())
+            # print(headers)
+
+            # Spreadsheet Writer Function Call
+            writer(spreadsheet_id, test_name,
+                   headers, resultDataframe)
+
+            # Adding Result Folder ID to generatedReports.txt (if only the reports are Generated)...
+            generatedReports.append(test_name)
+            file = open('.\credentials\generatedReports.txt', 'a')
+            file.write(f"{directoryID}\n")
+            file.close()
+
+    except Exception as error:
+        print("Error in Driver Code::: ", error)
+        pass
+
+if (reportGenerationCount == 0):
+    print("\nNo New Reports Generated")
+elif ((reportGenerationCount > 0) or len(generatedReports)):
+    print("\nGenerated Reports of :: {0}".format(generatedReports))
---
+++ sheet_writer.py
@@ -0,0 +1,110 @@
+from googleapiclient.discovery import build  # for building service
+# to work with service service_account
+from google.oauth2 import service_account
+from service_creator import Create_Service, Create_Service2
+from googleapiclient.errors import HttpError
+from datetime import datetime
+
+# downloaded from console and stored in the working discovery
+# SERVICE_ACCOUNT_FILE = 'service-account.json'
+SERVICE_ACCOUNT_FILE = '.\credentials\service-account.json'
+DRIVE_SCOPE = ['https://www.googleapis.com/auth/drive']
+SHEET_SCOPE = ['https://www.googleapis.com/auth/spreadsheets']
+SHEETS_API_NAME = 'sheets'
+SHEETS_API_VERSION = 'v4'
+DRIVE_API_NAME = 'drive'
+DRIVE_API_VERSION = 'v3'
+
+# creds = service_account.Credentials.from_service_account_file(
+#     SERVICE_ACCOUNT_FILE, scopes=SHEET_SCOPE)
+
+creds = service_account.Credentials.from_service_account_file(
+    SERVICE_ACCOUNT_FILE, scopes=SHEET_SCOPE)
+
+
+def writer(SPREADSHEET_ID, sheet_title, headers, resultArray):
+    try:
+        sheetservice = build(
+            SHEETS_API_NAME, SHEETS_API_VERSION, credentials=creds)
+        sheet = sheetservice.spreadsheets()  # just making it short
+
+        # --------------writing values to the cells of Sheet2
+        sheetValues = []
+        timestamp = f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
+
+        # Formatting Sheets Headers & Titles...
+        headerLength = len(headers)
+        testNameValue = ['Test Name :', sheet_title, '',
+                         'Report Generation Time', timestamp]
+        lineBreakValue = []
+
+        for x in range(0, headerLength):
+            if (x < (headerLength-5)):
+                testNameValue.append('')
+
+            lineBreakValue.append('----------')
+
+        sheetValues.append(testNameValue)
+        sheetValues.append(headers)
+
+        # Creating A List containing only Values from A List of Dictionaries...
+        resultValues = [list(result.values()) for result in resultArray]
+
+        # Concatenate Operation between Two Lists...
+        sheetValues = sheetValues + resultValues
+
+        # Adding Line Break Between Spreadsheet Reports...
+        sheetValues.append(lineBreakValue)
+
+        # write data values into the cells starting from A1 of Sheet1
+
+        # Dynamic Assignment of Spreadsheet Column Range...
+        rangeValue = f"Sheet1!A1:A{headerLength}"
+
+        sheet.values().append(spreadsheetId=SPREADSHEET_ID, range=f"{rangeValue}",
+                              valueInputOption="RAW", body={"values": sheetValues}).execute()
+
+        print("\nPopulating Google Spreadsheet for Test Name::: [ {0} ]... OK!".format(
+            sheet_title))
+        return
+
+    except HttpError as error:
+        print("Error in Sheet Writer Code::: ", error)
+        pass
+
+
+def sharer(ID):
+    try:
+        # share-link code starts here.
+        file_id = ID
+
+        # setting the permission attributes
+        request_body = {
+            'role': 'reader',
+            'type': 'anyone'
+        }
+        # driveservice = Create_Service2(
+        #     SERVICE_ACCOUNT_FILE, DRIVE_API_NAME, DRIVE_API_VERSION, DRIVE_SCOPE)
+
+        driveservice = Create_Service2(
+            SERVICE_ACCOUNT_FILE, DRIVE_API_NAME, DRIVE_API_VERSION, DRIVE_SCOPE)
+
+        response_permission = driveservice.permissions().create(
+            fileId=file_id, body=request_body, supportsAllDrives=True).execute()
+        # print(response_permission)
+
+        # Print Sharing Link
+        response_share_link = driveservice.files().get(
+            fileId=file_id,
+            fields='webViewLink',
+            supportsAllDrives=True).execute()
+
+        # print("The share link is : ",response_share_link)
+        # print("\nGenerating the URL... OK!")
+
+        # getting just the link from the object response_share_link
+        link = response_share_link.get("webViewLink")
+        return link
+    except Exception as error:
+        print("Error in Link Generation [Sharer] Code::: ", error)
+        pass
